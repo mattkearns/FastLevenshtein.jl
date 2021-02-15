aeb6e54dc454c57e89b81ca625258f5d2de8a505
Matt Kearns <mattkearns@protonmail.com>
2021-02-15 01:51:09 -0500

    add initial files

--- src/FastLevenshtein.jl
+++ src/FastLevenshtein.jl
@@ -1,5 +1,51 @@
 module FastLevenshtein
 
-# Write your package code here.
+    function fastlevenshtein(v1::Vector, v2::Vector)::Int64
+        """
+        Efficiently Compute the Levenshtein (edit) distance between two vectors using dynamic programming.
+        :param v1: The first vector is a Mx1 vector.
+        :param v2: The second vector is a Nx1 vector.
+        :return: The minimum number of edits required to make the vectors the same.
+        """
+        M::Int64, N::Int64 = length(v1), length(v2)
 
+        # This table holds the subproblems we have to solve to get an answer
+        table::Array{Int64} = zeros(Int64, (M+1, N+1))
+
+        for i::Int64 ∈ 0:M
+            for j::Int64 ∈ 0:N
+                # if the length of the 1st vector is 0 then we need at least j edits
+                if i == 0
+                    table[i+1, j+1] = j
+                # if the length of the 2nd vector is 0 then we need at least i edits
+                elseif j == 0
+                    table[i+1, j+1] = i
+                # if the values are the same then the minumum number of edits hasn't changed
+                elseif v1[i] == v2[j]
+                    table[i+1, j+1] = table[i, j]
+                # if the value are different then the minimum edits has increased by 1
+                else
+                    table[i+1, j+1] = 1 + min(
+                        table[i+1, j], # The minimum edits required if i > j (insert value @ current index)
+                        table[i, j+1], # The minimum edits required if i < j (remove value @ current index)
+                        table[i, j]    # The minimum edits required if i = j (update value @ current index)
+                    )
+                end
+            end
+        end
+
+        return table[M+1, N+1]
+    end
+
+    function fastlevenshtein(s1::AbstractString, s2::AbstractString)
+        """
+        Efficiently Compute the Levenshtein (edit) distance between two strings using dynamic programming.
+        :param s1: The first string contains M characters.
+        :param s2: The second string contains N characters.
+        :return: The minimum number of edits required to make the strings the same.
+        """
+        return fastlevenshtein(Vector{Char}(s1), Vector{Char}(s2))
+    end
+
+    export fastlevenshtein
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,44 @@
 using FastLevenshtein
 using Test
 
-@testset "FastLevenshtein.jl" begin
-    # Write your tests here.
-end
+@testset "The Levenshtein distance should be 0 if..." begin
+    @testset "the sequences are empty." begin
+        @test fastlevenshtein("", "") == 0
+        @test fastlevenshtein([], []) == 0
+    end
+    @testset "the sequences are the same." begin
+        @test fastlevenshtein("a", "a") == 0
+        @test fastlevenshtein("abc", "abc") == 0
+        @test fastlevenshtein([1], [1]) == 0
+        @test fastlevenshtein([1, 2, 3], [1, 2, 3]) == 0
+    end
+end;
+
+@testset "The Levenshtein distance should be 1 if..." begin
+    @testset "we only need to insert 1 value." begin
+        @test fastlevenshtein("abc", "ab") == 1
+        @test fastlevenshtein("abc", "ac") == 1
+        @test fastlevenshtein("abc", "bc") == 1
+        @test fastlevenshtein([1, 2, 3], [1, 2]) == 1
+        @test fastlevenshtein([1, 2, 3], [1, 3]) == 1
+        @test fastlevenshtein([1, 2, 3], [2, 3]) == 1
+    end
+    @testset "we only need to update 1 value." begin
+        @test fastlevenshtein("abc", "abd") == 1
+        @test fastlevenshtein("abc", "adc") == 1
+        @test fastlevenshtein("abc", "dbc") == 1
+        @test fastlevenshtein([1, 2, 3], [1, 2, 4]) == 1
+        @test fastlevenshtein([1, 2, 3], [1, 4, 3]) == 1
+        @test fastlevenshtein([1, 2, 3], [4, 2, 3]) == 1
+    end
+    @testset "we only need to remove 1 value." begin
+        @test fastlevenshtein("abc", "dabc") == 1
+        @test fastlevenshtein("abc", "adbc") == 1
+        @test fastlevenshtein("abc", "abdc") == 1
+        @test fastlevenshtein("abc", "abcd") == 1
+        @test fastlevenshtein([1, 2, 3], [4, 1, 2, 3]) == 1
+        @test fastlevenshtein([1, 2, 3], [1, 4, 2, 3]) == 1
+        @test fastlevenshtein([1, 2, 3], [1, 2, 4, 3]) == 1
+        @test fastlevenshtein([1, 2, 3], [1, 2, 3, 4]) == 1
+    end
+end;
